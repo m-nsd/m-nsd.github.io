3bba8930b2822f44458e3204b79be9a9029c1ee9
m-nsd <knsdgmknsd@gmail.com>
2024-02-07 10:44:54 +0900

    JS 11章まで済 JS gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,4 +1,31 @@
+// 画像ギャラリー機能を動かす
+// ここでやりたいのは、ギャラリーの各サムネイル画像にイベントリスナーをアタッチして
+// クリックされたときにメイン画像をサムネイル画像に対応するものに差し替えること
 function activateGallery() {
-    alert("ギャラリーファイルからこんにちは！");
-  }
-  
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage = document.querySelector("#gallery-photo > img");
+
+  //画像情報(右ペイン)の要素を補足し変数に入れておく
+  let galleryInfo = document.querySelector("#gallery-info");
+  let title       = galleryInfo.querySelector(".title");
+  let description = galleryInfo.querySelector(".description");
+
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function() {
+      // クリックされたサムネイル画像をメイン画像として設定する
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      mainImage.setAttribute("src", newImageSrc);
+
+      let newImageAlt = thumbnail.dataset.title;
+      mainImage.setAttribute("alt", newImageAlt);
+
+      //サムネイルの選択画像を変更する
+      document.querySelector(".current").classList.remove("current");
+      thumbnail.parentNode.classList.add("current");
+
+      //画像の情報(右ペイン)を、選択した画像に合わせて更新する
+      title.innerHTML       = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
+    });
+  });
+}
